7adf1bcf86edb9d69221fcb8155124ce8bb46650
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-01-06 00:01:44 +0100

    finir la structure et le style du header

---
+++ wp-content/themes/velvet-theme/front-page.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<main class="home-main">
+
+  <section class="hero">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/media/hero.jpg" alt="Velvet Company">
+  </section>
+
+</main>
+
+<?php get_footer(); ?>
